923b3b0fc5cb8244a422e13ba03bd32d3f2d6582
Nika <nika@nikableh.moe>
2025-08-17 14:12:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Conny
+
+Use OpenVPN with pleasure.
+
+## Goals
+
+### Technical
+
+- [ ] Successful VPN initialization
+- [ ] Save profiles
+- [ ] Work in background
+- [ ] Working 2FA
+- [ ] Ability to add custom console options
+- [ ] Ability to view logs
+- [ ] Color messages of different log levels in logs
+
+### Infrustructure
+
+- [ ] Package with Nix
+- [ ] Package with Flatpak
+- [ ] Apply for GNOME Circle
+- [ ] Publish to Flathub
+
+## License
+
+This project is under the GPL-3.0-only license.
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Nix flake of the project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rustToolchain =
+          (fenix.packages.${system}.fromToolchainName {
+            name = (pkgs.lib.importTOML ./rust-toolchain.toml).toolchain.channel;
+            sha256 = "sha256-+9FmLhAOezBZCOziO0Qct1NOrfpjNsXxc/8I0c7BdKE=";
+          }).toolchain;
+      in
+      {
+        formatter = pkgs.nixfmt-rfc-style;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.bashInteractive
+            pkgs.gtk4
+            pkgs.pkg-config
+          ];
+        };
+      }
+    );
+}
