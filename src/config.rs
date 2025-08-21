f012556d714867d8331c6fda8365dce27dae3ace
Nika <nika@nikableh.moe>
2025-08-21 15:31:47 +0300

    Set RESOURCES_FILE env based on build profile

--- src/config.rs
+++ src/config.rs
@@ -4,5 +4,5 @@ pub const GETTEXT_PACKAGE: &str = env!("GETTEXT_PACKAGE");
 pub const LOCALEDIR: &str = env!("LOCALEDIR");
 pub const PKGDATADIR: &str = env!("PKGDATADIR");
 pub const PROFILE: &str = env!("PROFILE");
-pub const RESOURCES_FILE: &str = concat!(env!("PKGDATADIR"), "/resources.gresource");
+pub const RESOURCES_FILE: &str = env!("RESOURCES_FILE");
 pub const VERSION: &str = env!("VERSION");
